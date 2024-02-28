45564aec43f90b523ed6de5aa78dbc529c51f7b5
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-28 14:48:51 -0300

    Section 02: Pure React - Lessons 01 to 05

---
+++ src/App.js
@@ -0,0 +1,22 @@
+const Pet = ({name, animal, breed}) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed)
+    ]);
+};
+
+const App = () => {
+            return React.createElement(
+                "div",
+                { id: "something-important"},
+                [
+                    React.createElement("h1", {}, "Adopt Me!"),
+                    React.createElement(Pet, {name: "Pandora", animal: "Dog", breed: "Mixed"}),
+                    React.createElement(Pet, {name: "Cacau", animal: "Dog", breed: "Mixed"}),
+                    React.createElement(Pet, {name: "Toy", animal: "Cat", breed: "Mixed"}),
+                ]
+            );
+        };
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
