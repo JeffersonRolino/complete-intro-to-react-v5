2c655067b40929cac1503b8915a06066998cbe73
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-29 15:36:09 -0300

    Section 05: Hooks - Lessons 01 and 02: Setting State with Hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet.js";
+import SearchParams from "./SearchParams.js";
 
 const App = () => {
     return (
         <div>
             <h1>Adopt Me!</h1>
-            <Pet name="Pandora" animal="Dog" breed="Mixed" />
-            <Pet name="Cacau" animal="Dog" breed="Mixed" />
-            <Pet name="Toy" animal="Cat" breed="Mixed" />
+            <SearchParams />
         </div>
     );
 };
