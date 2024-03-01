359e237fb0730cc44b9638668635e9001a76693d
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-01 05:56:26 -0300

    Section 05: Hooks - Lessons 05 to 08: Custom Hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropDown from "./useDropDown";
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
+    // const [animal, setAnimal] = useState("dog");
+    // const [breed, setBreed] = useState("");
+    const [breeds, setBreeds] = useState([]);
+    const [animal, AnimalDropdown] = useDropDown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown] = useDropDown("Breed", "", breeds);
 
     return (
         <div className="search-params">
@@ -15,6 +22,10 @@ const SearchParams = () => {
                         onChange={(event) => setLocation(event.target.value)}
                     />
                 </label>
+
+                <AnimalDropdown />
+                <BreedDropdown />
+
                 <button>Submit</button>
             </form>
         </div>
